852b5fc500fd37ed2e59c0303b390b5c9b7a8528
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-31 14:36:15 -0400

    importing browser router and made other components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+// import styles
+import './styles/index.css';
+
+// import React Router
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+// import components
+import App from './components/App.js';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
